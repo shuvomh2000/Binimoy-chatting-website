4bf708b62795ce39a51a3b39a88bcca46f109bbf
Shuvo MH <96376185+shuvomh2000@users.noreply.github.com>
2023-01-09 00:13:36 +0600

    friend request

--- src/components/FriendRequest.js
+++ src/components/FriendRequest.js
@@ -1,45 +1,78 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
+import { getDatabase, ref, onValue } from "firebase/database";
+import { getAuth } from "firebase/auth";
+import {RxCross2} from "react-icons/rx"
+import {AiOutlineCheck} from "react-icons/ai"
 
 const FriendRequest = () => {
+  const db = getDatabase();
+  const auth = getAuth();
+
+  let [friendRequests, setFriendRequests] = useState([]);
+
+  useEffect(() => {
+    // const starCountRef = ref(db, "friendrequest/" );
+    // onValue(starCountRef, (snapshot) => {
+    //   const data = snapshot.val();
+    //   updateStarCount(postElement, data);
+    // });
+
+    const friendRequestsRef = ref(db, "friendrequest/");
+    onValue(friendRequestsRef, (snapshot) => {
+
+    let arr = [];
+    snapshot.forEach((item) => {
+      if (item.val().recever_id == auth.currentUser.uid) {
+        arr.push(item.val());
+      }
+    });
+    setFriendRequests(arr);
+    });
+  }, []);
+
   return (
     <div className="shadow-md p-[20px] rounded-[20px]">
-    <div>
       <div className="flex justify-between ">
         <h3 className="font-poppins text-black text-xl font-semibold capitalize">
-         friend request
+          friend request
         </h3>
         <button>...</button>
       </div>
-
-      <div className=" overflow-y-auto max-h-[380px]">
-          {/*  */}
-        <div className="flex justify-between py-[10px] border-b border-solid">
-          <div className="flex">
-            <div className="w-[55px] h-[55px] rounded-[50%]">
-              <picture>
-                <img src="images/user1.png" />
-              </picture>
+      {friendRequests.map((item) => (
+        <div>
+          <div className=" overflow-y-auto max-h-[380px]">
+            {/*  */}
+            <div className="flex justify-between py-[10px] border-b border-solid">
+              <div className="flex">
+                <div className="w-[55px] h-[55px] rounded-[50%]">
+                  <picture>
+                    <img src="images/user1.png" />
+                  </picture>
+                </div>
+                <div className="ml-[10px] flex items-center">
+                  <h4 className="font-poppins text-black text-sm font-semibold capitalize">
+                    {item.sender_name}
+                  </h4>
+                  {/* <p className="font-poppins text-msg text-sm font-normal">
+                    Dinner
+                  </p> */}
+                </div>
+              </div>
+              <div className="flex gap-x-[10px]  items-center">
+                <button className=" bg-red text-white w-[25px] h-[25px] flex justify-center items-center rounded font-normal text-xl capitalize ">
+                  <RxCross2/>
+                </button>
+                <button className="bg-green text-white w-[25px] h-[25px] rounded font-normal text-xl  capitalize flex justify-center items-center">
+                <AiOutlineCheck/>
+                </button>
+              </div>
             </div>
-            <div className="ml-[10px] mt-[7px]">
-              <h4 className="font-poppins text-black text-sm font-semibold capitalize">
-                Raghav
-              </h4>
-              <p className="font-poppins text-msg text-sm font-normal">
-                Dinner
-              </p>
-            </div>
-          </div>
-          <div>
-            <button className="bg-primary text-white px-[8px] pb-[3px] rounded font-normal text-xl mt-[10px] capitalize">
-              accept
-            </button>
+            {/*  */}
           </div>
         </div>
-        {/*  */}
-      </div>
+      ))}
     </div>
-  </div>
-  )
-}
+  );
+};
 
-export default FriendRequest
+export default FriendRequest;
--- src/components/UserList.js
+++ src/components/UserList.js
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from "react";
-import { getDatabase, ref, onValue } from "firebase/database";
+import { getDatabase, ref, onValue,set,push  } from "firebase/database";
 import { getAuth } from "firebase/auth";
+import {AiOutlinePlus} from 'react-icons/ai'
 
 const UserList = () => {
   const db = getDatabase();
@@ -11,15 +12,24 @@ const UserList = () => {
     const usersRef = ref(db, "users/");
     onValue(usersRef, (snapshot) => {
       let arr = [];
-      snapshot.forEach((item)=>{
-        if(item.key !== auth.currentUser.uid){
-          arr.push(item.val())
+      snapshot.forEach((item) => {
+        if (item.key !== auth.currentUser.uid) {
+          arr.push({...item.val(),id:item.key});
         }
-      })
-      setUserList(arr)
-      console.log(arr)
+      });
+      setUserList(arr);
     });
   }, []);
+
+  // send request
+  let handleSendFriendRequest = (item) => {
+    set(push(ref(db, "friendrequest")), {
+      sender_id:auth.currentUser.uid,
+      sender_name:auth.currentUser.displayName,
+      recever_id:item.id,
+      recever_name:item.name
+    });
+  };
 
   return (
     <div className="shadow-md p-[20px] rounded-[20px]">
@@ -33,31 +43,34 @@ const UserList = () => {
 
         <div className=" overflow-y-auto max-h-[380px]">
           {/*  */}
-          {userList.map((item)=>(
+          {userList.map((item) => (
             <div className="flex justify-between py-[10px] border-b border-solid">
-            <div className="flex">
-              <div className="w-[55px] h-[55px] rounded-[50%] overflow-hidden">
-                <picture>
-                  {/* <img src="images/user2.png" /> */}
-                  <img src={item.photoURL} />
-                </picture>
-              </div>
-              <div className="ml-[10px] mt-[7px]">
-                <h4 className="font-poppins text-black text-sm font-semibold capitalize">
-                  {item.name}
-                  {/* adgfggfd */}
-                </h4>
-                {/* <p className="font-poppins text-msg text-sm font-normal">
+              <div className="flex">
+                <div className="w-[55px] h-[55px] rounded-[50%] overflow-hidden">
+                  <picture>
+                    {/* <img src="images/user2.png" /> */}
+                    <img src={item.photoURL} />
+                  </picture>
+                </div>
+                <div className="ml-[10px] flex items-center">
+                  <h4 className="font-poppins text-black text-sm font-semibold capitalize">
+                    {item.name}
+                    {/* adgfggfd */}
+                  </h4>
+                  {/* <p className="font-poppins text-msg text-sm font-normal">
                   {item.email}
                 </p> */}
+                </div>
+              </div>
+              <div className="flex items-center">
+                <button
+                  onClick={() => handleSendFriendRequest(item)}
+                  className="bg-primary text-white w-[25px] h-[25px] flex justify-center items-center rounded font-normal text-xl"
+                >
+                  <AiOutlinePlus/>
+                </button>
               </div>
             </div>
-            <div>
-              <button className="bg-primary text-white px-[8px] pb-[3px] rounded font-normal text-xl mt-[10px]">
-                <p>+</p>
-              </button>
-            </div>
-          </div>
           ))}
           {/*  */}
         </div>
