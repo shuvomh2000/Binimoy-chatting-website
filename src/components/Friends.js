3d99aecd470c058e3936fa28789bfe929b0c83a1
Shuvo MH <96376185+shuvomh2000@users.noreply.github.com>
2023-01-14 20:31:43 +0600

    home page done

--- src/components/Friends.js
+++ src/components/Friends.js
@@ -9,7 +9,7 @@ import {
 } from "firebase/database";
 import { getAuth } from "firebase/auth";
 
-const Friends = () => {
+const Friends = (props) => {
   const db = getDatabase();
   const auth = getAuth();
 
@@ -20,7 +20,6 @@ const Friends = () => {
     onValue(friendsRef, (snapshot) => {
       let arr = [];
       snapshot.forEach((item) => {
-        // console.log({...item.val(),id:item.key})
         if (
           item.val().acceptId == auth.currentUser.uid ||
           item.val().senderId == auth.currentUser.uid
@@ -34,15 +33,15 @@ const Friends = () => {
 
   let handleBlock = (item) => {
     set(push(ref(db, "blockUsers")), {
-      block:item.sendername,
-      blockid:item.senderId,
-      blockby:auth.currentUser.displayName,
-      blockbyid:auth.currentUser.uid,
-      key:item.id
-    }).then(()=>{
+      block: item.sendername,
+      blockid: item.senderId,
+      blockby: auth.currentUser.displayName,
+      blockbyid: auth.currentUser.uid,
+      key: item.id,
+    }).then(() => {
       remove(ref(db, "friends/" + item.id));
-    })
-  }
+    });
+  };
 
   return (
     <div className="shadow-md mt-[30px] p-[20px] rounded-[20px]">
@@ -58,31 +57,37 @@ const Friends = () => {
           {/*  */}
           {friendRequests.map((item) => (
             <div className="flex justify-between py-[10px] border-b border-solid last:border-0">
-            <div className="flex">
-              <div className="w-[55px] h-[55px] rounded-[50%] overflow-hidden">
-                <picture>
-                  {/* <img src="images/user2.png" /> */}
-                  <img src="images/user2.jpg" />
-                </picture>
-              </div>
-              <div className="ml-[10px] flex items-center">
-                <h4 className="font-poppins text-black text-sm font-semibold capitalize">
-                  {item.acceptId == auth.currentUser.uid ?
-                    item.sendername:item.acceptname}
-                </h4>
-                {/* <p className="font-poppins text-msg text-sm font-normal">
+              <div className="flex">
+                <div className="w-[55px] h-[55px] rounded-[50%] overflow-hidden">
+                  <picture>
+                    {/* <img src="images/user2.png" /> */}
+                    <img src="images/user2.jpg" />
+                  </picture>
+                </div>
+                <div className="ml-[10px] flex items-center">
+                  <h4 className="font-poppins text-black text-sm font-semibold capitalize">
+                    {item.acceptId == auth.currentUser.uid
+                      ? item.sendername
+                      : item.acceptname}
+                  </h4>
+                  {/* <p className="font-poppins text-msg text-sm font-normal">
                 {item.email}
               </p> */}
+                </div>
               </div>
+              {props.btn ? (
+                ""
+              ) : (
+                <div className="flex items-center">
+                  <button
+                    onClick={() => handleBlock(item)}
+                    className="bg-bl_opacity text-white px-[8px] pb-[3px] rounded font-normal text-md mt-[10px] capitalize"
+                  >
+                    block
+                  </button>
+                </div>
+              )}
             </div>
-            <div className="flex items-center">
-                <button onClick={()=>handleBlock(item)}
-                  className="bg-bl_opacity text-white px-[8px] pb-[3px] rounded font-normal text-md mt-[10px] capitalize"
-                >
-                  block
-                </button>
-            </div>
-          </div>
           ))}
           {/*  */}
         </div>
--- src/components/message/index.js
+++ src/components/message/index.js
@@ -1,15 +1,132 @@
-import React from 'react'
-import Sidebar from '../Sidebar'
+import React, { useEffect, useState } from "react";
+import Search from "../Search";
+import Sidebar from "../Sidebar";
+import {
+  getDatabase,
+  ref,
+  onValue,
+  set,
+  push,
+  remove,
+} from "firebase/database";
+import { getAuth } from "firebase/auth";
+import Friends from "../Friends";
 
 const Message = () => {
-  return (
-    <div className='flex justify-between'>
-      <div className='w-[14%]'>
-        <Sidebar active="message"/>
-      </div>
-      <div className='w-[84%]'></div>
-    </div>
-  )
-}
+  const db = getDatabase();
+  const auth = getAuth();
 
-export default Message
+  let [mygrp, setMygrp] = useState([]);
+  let [grpmember, setGrpMember] = useState([]);
+
+  useEffect(() => {
+    const friendRequestsRef = ref(db, "groups/");
+    onValue(friendRequestsRef, (snapshot) => {
+      let arr = [];
+      snapshot.forEach((item) => {
+        if (item.val().admin == auth.currentUser.uid) {
+          arr.push({ ...item.val(), id: item.key });
+        }
+      });
+      setMygrp(arr);
+    });
+  }, []);
+
+  useEffect(() => {
+    const friendRequestsRef = ref(db, "groupmembers/");
+    onValue(friendRequestsRef, (snapshot) => {
+      let arr = [];
+      snapshot.forEach((item) => {
+        if (item.val().userid == auth.currentUser.uid) {
+          arr.push({ ...item.val(), id: item.key });
+        }
+      });
+      setGrpMember(arr);
+    });
+  }, []);
+
+  return (
+    <div className="flex justify-between">
+      <div className="w-[14%]">
+        <Sidebar active="message" />
+      </div>
+      <div className="w-[84%]">
+        <div className="w-[30%] h-screen flex flex-wrap">
+          <div className="w-full">
+            <Search />
+            <Friends btn={true} />
+            {/* <div className="shadow-md mt-[30px] p-[20px] rounded-[20px]">
+              <div className="max-h-[295px] overflow-y-auto">
+              </div>
+            </div> */}
+          </div>
+          <div className="w-full">
+            <div className="shadow-md p-[20px] rounded-[20px]">
+              <div className="max-h-[295px] overflow-y-auto">
+                <div className="flex justify-between ">
+                  <h3 className="font-poppins text-black text-xl font-semibold capitalize">
+                    my groups
+                    {/* <span className="font-normal text-[16px]">
+                      ({grp.length})
+                    </span> */}
+                  </h3>
+                  <button>
+                  </button>
+                </div>
+                {mygrp.map((item) => (
+                  <div>
+                    <div className=" overflow-y-auto max-h-[380px]">
+                      {/*  */}
+                      <div className="flex justify-between py-[10px] border-b border-solid last:border-0">
+                        <div className="flex">
+                          <div className="w-[55px] h-[55px] rounded-[50%]">
+                            <picture>
+                              <img src="images/user1.png" />
+                            </picture>
+                          </div>
+                          <div className="ml-[10px] flex items-center">
+                            <h4 className="font-poppins text-black text-sm font-semibold capitalize">
+                              {item.groupName}
+                            </h4>
+                          </div>
+                        </div>
+                      </div>
+                      {/*  */}
+                    </div>
+                  </div>
+                ))}
+                {grpmember.map((item) => (
+                  <div>
+                    <div className=" overflow-y-auto max-h-[380px]">
+                      {/*  */}
+                      <div className="flex justify-between py-[10px] border-b border-solid last:border-0">
+                        <div className="flex">
+                          <div className="w-[55px] h-[55px] rounded-[50%]">
+                            <picture>
+                              <img src="images/user1.png" />
+                            </picture>
+                          </div>
+                          <div className="ml-[10px] flex items-center">
+                            <h4 className="font-poppins text-black text-sm font-semibold capitalize">
+                              {item.gName}
+                            </h4>
+                            {/* <p className="font-poppins text-msg text-sm font-normal">
+                    Dinner
+                  </p> */}
+                          </div>
+                        </div>
+                      </div>
+                      {/*  */}
+                    </div>
+                  </div>
+                ))}
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Message;
